6223a1541ecaf40de8163a2c3fc3caf188c08c04
leniagata0 <leniagata0@gmail.com>
2025-04-24 03:22:02 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 
 const AdScript = () => (
-  <Script src="https://labtasum.blogspot.com"/>
+  <iframe src="https://labtasum.blogspot.com"/>
 );
 
 export default AdScript;

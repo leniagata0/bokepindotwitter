7fc1ec5094e47b89a92cf1fe4bbe544ba9fe6d7a
leniagata0 <leniagata0@gmail.com>
2025-04-24 03:50:13 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,9 +1,42 @@
-import Script from 'next/script';
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
-const AdScript = () => (
-  <script async='async' data-cfasync='false' src='https://poweredby.jads.co/js/jads.js' type='text/javascript'></script>
-<ins data-height='250' data-width='300' id='718531'></ins>
-<script async='async' data-cfasync='false' type='text/javascript'>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':718531});</script>
-);
+class AdCodeWithoutRouter extends React.Component {
+  renderAds() {
+    (window.adsbyjuicy = window.adsbyjuicy || []).push({});
+  }
+
+  componentDidMount() {
+    this.renderAds();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.router.asPath !== prevProps.router.asPath) {
+      this.renderAds();
+    }
+  }
+
+  render() {
+    return (
+      <div className="container mx-auto text-center" aria-hidden={true}>
+        <ins
+          className="adsbygoogle"
+          id="718531"
+          data-height="250"
+          data-width="300"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <script dangerouslySetInnerHTML={{ __html: '(window.adsbyjuicy = window.adsbyjuicy || []).push({'adzone':718531});' }}></script>
+      </div>
+    );
+  }
+}
+
+const AdScript = () => {
+  const router = useRouter();
+  return <AdCodeWithoutRouter router={router} />;
+};
 
 export default AdScript;

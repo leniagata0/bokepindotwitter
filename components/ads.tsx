7cbd029bbc3f8ca913a071391f14c37edf31a03a
leniagata0 <leniagata0@gmail.com>
2025-04-24 03:27:36 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,9 @@
 import Script from 'next/script';
 
 const AdScript = () => (
-  <iframe height="100" width="300" src="https://labtasum.blogspot.com"/>
+  <script async='async' data-cfasync='false' src='https://poweredby.jads.co/js/jads.js' type='text/javascript'></script>
+<ins data-height='250' data-width='300' id='718531'></ins>
+<script async='async' data-cfasync='false' type='text/javascript'>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':718531});</script>
 );
 
 export default AdScript;
